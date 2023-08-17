429468765a7b3f1789b9e0e491d6a2dd895a3d22
marcoborelli <marco05.borelli@gmail.com>
2023-08-17 20:41:04 +0200

    utilities\FormManager: aggiunta classe statica contenente controlli comuni tra form per scopi futuri.

---
+++ Borelli_BdT/utilities/FormManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Borelli_BdT.utilities {
+    public static class FormManager {
+        private static MaterialSkinManager MaterialSkinManager { get; set; }
+        public static void Init() {
+            MaterialSkinManager = MaterialSkinManager.Instance;
+
+            MaterialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            MaterialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        public static void AddForm(MaterialForm f) {
+            MaterialSkinManager.AddFormToManage(f);
+        }
+
+        public static void ChangeTheme(bool theme) {
+            MaterialSkinManager.Theme = theme ? MaterialSkinManager.Themes.LIGHT : MaterialSkinManager.Themes.DARK;
+        }
+    }
+}
