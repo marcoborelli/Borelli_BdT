2bce83e9dca14f075b6d1eaa1aea2b23d9661884
marcoborelli <marco05.borelli@gmail.com>
2023-08-20 16:50:36 +0200

    utilities\Parameters: cambiata logica. Ora i nomi dei file json sono fissi, a cambiare è il path di base in cui possono essere salvati. Rimossi attributi non statici non più necessari. Gli attributi statici vengono inizializzati in Init basandosi sul path. FP = FilePath DP = DirectoryPath

--- Borelli_BdT/utilities/Parameters.cs
+++ Borelli_BdT/utilities/Parameters.cs
@@ -1,91 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Borelli_BdT.model;
 
 namespace Borelli_BdT.utilities {
     public /*static*/ class Parameters {
-        private static string _FPUsers, _FPTasks, _FPDistricts, _FPJobs;
         private static float _deltaPercentage;
+        private static string _parametersPath, _DPBase;
+        public static string FPUsers { get; private set; }
+        public static string FPTasks { get; private set; }
+        public static string FPDistricts { get; private set; }
+        public static string FPJobs { get; private set; }
+        public static string DPPictures { get; private set; }
 
-        private static string _parametersPath;
 
         //li ri-dichiaro non statici per poterli leggere da file Json e associarli successivamente a quelli statici
-        public string FilePathUser { get; private set; }
-        public string FilePathTasks { get; private set; }
-        public string FilePathDistricts { get; private set; }
-        public string FilePathJobs { get; private set; }
+        public string BasePath { get; private set; }
         public float DeltaPerc { get; private set; }
 
-        public Parameters(string filePathUser, string filePathTasks, string filePathDistricts, string filePathJobs, float deltaPerc) {
-            FilePathUser = filePathUser;
-            FilePathTasks = filePathTasks;
-            FilePathDistricts = filePathDistricts;
-            FilePathJobs = filePathJobs;
+        public Parameters(string basePath, float deltaPerc) {
+            BasePath = basePath;
             DeltaPerc = deltaPerc;
         }
 
         public static void Init(string path) {
             ParametersPath = path;
+
             Parameters tmp = FileManager.ReadParametersFile(ParametersPath);
 
-            FPUsers = tmp.FilePathUser;
-            FPTasks = tmp.FilePathTasks;
-            FPDistricts = tmp.FilePathDistricts;
-            FPJobs = tmp.FilePathJobs;
+            DPBase = tmp.BasePath;
             DeltaPercentage = tmp.DeltaPerc;
+
+            FPUsers = $"{DPBase}/users.json";
+            FPTasks = $"{DPBase}/tasks.json";
+            FPDistricts = $"{DPBase}/districts.json";
+            FPJobs = $"{DPBase}/jobs.json";
+            DPPictures = $"{DPBase}/pictures";
         }
+
+
 
         private static string ParametersPath {
             get => _parametersPath;
             set => DataChecker.SetIfValidString(ref _parametersPath, value, "Inserire un path valido", CheckStr.Generic);
         }
-        public static string FPUsers {
-            get => _FPUsers;
-            private set {
-                if (!DataChecker.IsValidString(value, CheckStr.Generic))
-                    throw new Exception("Inserire un percorso file per il file 'users.json' valido");
 
-                _FPUsers = value;
-            }
+
+        public static string DPBase {
+            get => _DPBase;
+            private set => DataChecker.SetIfValidString(ref _DPBase, value, "Inserire un nome per la cartella bse valido", CheckStr.Generic);
         }
 
-        public static string FPTasks {
-            get => _FPTasks;
-            private set {
-                if (!DataChecker.IsValidString(value, CheckStr.Generic))
-                    throw new Exception("Inserire un percorso file per il file 'tasks.json' valido");
-
-                _FPTasks = value;
-            }
-        }
-
-        public static string FPDistricts {
-            get => _FPDistricts;
-            private set {
-                if (!DataChecker.IsValidString(value, CheckStr.Generic))
-                    throw new Exception("Inserire un percorso file per il file 'districts.json' valido");
-
-                _FPDistricts = value;
-            }
-        }
-
-        public static string FPJobs {
-            get => _FPJobs;
-            private set {
-                if (!DataChecker.IsValidString(value, CheckStr.Generic))
-                    throw new Exception("Inserire un percorso file per il file 'jobs.json' valido");
-
-                _FPJobs = value;
-            }
-        }
         public static float DeltaPercentage {
             get => _deltaPercentage;
             private set => DataChecker.SetIfValidNumber(ref _deltaPercentage, value, "Inserire una percentuale [0-100]", CheckNumb.Percentage);
         }
 
         public static void WriteJsonFile() {
-            Parameters p = new Parameters(FPUsers, FPTasks, FPDistricts, FPJobs, DeltaPercentage);
+            Parameters p = new Parameters(DPBase, DeltaPercentage);
             FileManager.WriteJsonFile(p, ParametersPath);
         }
     }
