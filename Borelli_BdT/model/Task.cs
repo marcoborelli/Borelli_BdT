a309b41770622cf006b45ed27b75ca4bf68a4bfe
marcoborelli <marco05.borelli@gmail.com>
2023-08-20 16:42:53 +0200

    model\Task: aggiunti campi [JsonProperty] e aggiunto controllo sui set che impostano solo se ci si trova nella fase giusta o in una successiva.

--- Borelli_BdT/model/Task.cs
+++ Borelli_BdT/model/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Borelli_BdT.utilities;
+using Newtonsoft.Json;
 
 namespace Borelli_BdT.model {
     public enum TPhase {
@@ -11,62 +12,104 @@ namespace Borelli_BdT.model {
     public class Task : IEquatable<Task> {
         private string _requesterNickname, _acceptorNickname;
 
-        public DateTime RequestDate { get; private set; }
-        public DateTime AcceptedTaskDate { get; private set; }
-        public DateTime StartTaskDate { get; private set; }
-        private DateTime _endTaskDate;
-        private TimeSpan _taskLength;
+        [JsonProperty]
         public TPhase Status { get; private set; }
+
+        [JsonProperty]
+        public DateTime RequestDate { get; private set; }
+        private DateTime _acceptedTaskDate, _startTaskDate, _endTaskDate;
+        private TimeSpan _taskLength;
         private float _stars;
         private string _job, _id;
 
-        public string RequesterNickname {
-            get => _requesterNickname;
-            private set => DataChecker.SetIfValidString(ref _requesterNickname, value, "Inserire un nickname di un richiedente associato ad un utente", CheckStr.Nickname);
-        }
-        public string AcceptorNickname {
-            get => _acceptorNickname;
-            private set {
-                if (value != RequesterNickname) {
-                    DataChecker.SetIfValidString(ref _acceptorNickname, value, "Inserire un nickname di un accettatore associato ad un utente", CheckStr.Nickname);
-                } else {
-                    throw new Exception("Il richiedente non può essere uguale a colui che accetta");
-                }
-            }
-        }
-        public DateTime EndTaskDate {
-            get => _endTaskDate;
-            private set {
-                if (!(value.Date < StartTaskDate)) {
-                    _endTaskDate = value;
-                } else {
-                    throw new Exception($"Inserire una data valida di terminazione del lavoro");
-                }
-            }
-        }
-        public TimeSpan TaskLength {
-            get => _taskLength;
-            private set {
-                if (value.TotalHours < (EndTaskDate - StartTaskDate).TotalHours) {
-                    _taskLength = value;
-                } else {
-                    throw new Exception($"Inserire una durata valida del lavoro");
-                }
-            }
-        }
-        public float Stars {
-            get => _stars;
-            private set => DataChecker.SetIfValidNumber(ref _stars, value, "Inserire un corretto numero di stelle [0-5]", CheckNumb.Stars);
-        }
-        public string Job {
-            get => _job;
-            private set => DataChecker.SetIfValidString(ref _job, value, "Inserire un tipo di lavoro valido", CheckStr.Work);
-        }
+        [JsonProperty]
         public string Id {
             get => _id;
             private set => DataChecker.SetIfValidString(ref _id, value, "Errore durante l'assegnazione dell'id", CheckStr.Generic);
         }
 
+        [JsonProperty]
+        public string Job {
+            get => _job;
+            private set => DataChecker.SetIfValidString(ref _job, value, "Inserire un tipo di lavoro valido", CheckStr.Work);
+        }
+
+        [JsonProperty]
+        public string RequesterNickname {
+            get => _requesterNickname;
+            private set => DataChecker.SetIfValidString(ref _requesterNickname, value, "Inserire un nickname di un richiedente associato ad un utente", CheckStr.Nickname);
+        }
+        [JsonProperty]
+        public string AcceptorNickname {
+            get => _acceptorNickname;
+            private set {
+                if ((int)Status >= (int)TPhase.Accepted) {
+                    if (value != RequesterNickname) {
+                        DataChecker.SetIfValidString(ref _acceptorNickname, value, "Inserire un nickname di un accettatore associato ad un utente", CheckStr.Nickname);
+                    } else {
+                        throw new Exception("Il richiedente non può essere uguale a colui che accetta");
+                    }
+                }
+            }
+        }
+
+        [JsonProperty]
+        public DateTime AcceptedTaskDate {
+            get => _acceptedTaskDate;
+            private set {
+                if ((int)Status >= (int)TPhase.Accepted) {
+                    _acceptedTaskDate = value;
+                }
+            }
+        }
+
+        [JsonProperty]
+        public DateTime StartTaskDate {
+            get => _startTaskDate;
+            private set {
+                if ((int)Status >= (int)TPhase.Accepted) {
+                    _startTaskDate = value;
+                }
+            }
+        }
+
+        [JsonProperty]
+        public DateTime EndTaskDate {
+            get => _endTaskDate;
+            private set {
+                if ((int)Status >= (int)TPhase.Done) {
+                    if (!(value.Date < StartTaskDate)) {
+                        _endTaskDate = value;
+                    } else {
+                        throw new Exception($"Inserire una data valida di terminazione del lavoro");
+                    }
+                }
+            }
+        }
+
+        [JsonProperty]
+        public TimeSpan TaskLength {
+            get => _taskLength;
+            private set {
+                if ((int)Status >= (int)TPhase.Done) {
+                    if (value.TotalHours < (EndTaskDate - StartTaskDate).TotalHours) {
+                        _taskLength = value;
+                    } else {
+                        throw new Exception($"Inserire una durata valida del lavoro");
+                    }
+                }
+            }
+        }
+
+        [JsonProperty]
+        public float Stars {
+            get => _stars;
+            private set {
+                if ((int)Status >= (int)TPhase.Done) {
+                    DataChecker.SetIfValidNumber(ref _stars, value, "Inserire un corretto numero di stelle [0-5]", CheckNumb.Stars);
+                }
+            }
+        }
 
         public Task() {
             Status = TPhase.Nothing;
