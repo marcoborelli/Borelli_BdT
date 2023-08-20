e43827e6439adb1608aae92687c5f696f2eedbc8
marcoborelli <marco05.borelli@gmail.com>
2023-08-20 16:58:55 +0200

    view\Login: creata grafica form. Aggiunti metodi per futura implementazione di una classe presenter.

--- Borelli_BdT/view/Login.Designer.cs
+++ Borelli_BdT/view/Login.Designer.cs
@@ -23,22 +23,129 @@
         /// il contenuto del metodo con l'editor di codice.
         /// </summary>
         private void InitializeComponent() {
+            this.mLabelLogin = new MaterialSkin.Controls.MaterialLabel();
+            this.mTextBoxUsername = new MaterialSkin.Controls.MaterialTextBox();
+            this.mTextBoxPasswd = new MaterialSkin.Controls.MaterialTextBox();
+            this.mButtonLogin = new MaterialSkin.Controls.MaterialButton();
+            this.mButtonSignUp = new MaterialSkin.Controls.MaterialButton();
             this.SuspendLayout();
-            //
-            // Form1
-            //
+            // 
+            // mLabelLogin
+            // 
+            this.mLabelLogin.AutoSize = true;
+            this.mLabelLogin.Depth = 0;
+            this.mLabelLogin.Font = new System.Drawing.Font("Roboto", 34F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.mLabelLogin.FontType = MaterialSkin.MaterialSkinManager.fontType.H4;
+            this.mLabelLogin.Location = new System.Drawing.Point(34, 38);
+            this.mLabelLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mLabelLogin.Name = "mLabelLogin";
+            this.mLabelLogin.Size = new System.Drawing.Size(84, 41);
+            this.mLabelLogin.TabIndex = 0;
+            this.mLabelLogin.Text = "Login";
+            // 
+            // mTextBoxUsername
+            // 
+            this.mTextBoxUsername.AnimateReadOnly = false;
+            this.mTextBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mTextBoxUsername.Depth = 0;
+            this.mTextBoxUsername.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.mTextBoxUsername.Hint = "Nickname";
+            this.mTextBoxUsername.LeadingIcon = null;
+            this.mTextBoxUsername.Location = new System.Drawing.Point(34, 132);
+            this.mTextBoxUsername.MaxLength = 50;
+            this.mTextBoxUsername.MouseState = MaterialSkin.MouseState.OUT;
+            this.mTextBoxUsername.Multiline = false;
+            this.mTextBoxUsername.Name = "mTextBoxUsername";
+            this.mTextBoxUsername.Size = new System.Drawing.Size(241, 50);
+            this.mTextBoxUsername.TabIndex = 1;
+            this.mTextBoxUsername.Text = "";
+            this.mTextBoxUsername.TrailingIcon = null;
+            // 
+            // mTextBoxPasswd
+            // 
+            this.mTextBoxPasswd.AnimateReadOnly = false;
+            this.mTextBoxPasswd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mTextBoxPasswd.Depth = 0;
+            this.mTextBoxPasswd.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.mTextBoxPasswd.Hint = "Password";
+            this.mTextBoxPasswd.LeadingIcon = null;
+            this.mTextBoxPasswd.Location = new System.Drawing.Point(34, 210);
+            this.mTextBoxPasswd.MaxLength = 50;
+            this.mTextBoxPasswd.MouseState = MaterialSkin.MouseState.OUT;
+            this.mTextBoxPasswd.Multiline = false;
+            this.mTextBoxPasswd.Name = "mTextBoxPasswd";
+            this.mTextBoxPasswd.Password = true;
+            this.mTextBoxPasswd.Size = new System.Drawing.Size(241, 50);
+            this.mTextBoxPasswd.TabIndex = 2;
+            this.mTextBoxPasswd.Text = "";
+            this.mTextBoxPasswd.TrailingIcon = null;
+            // 
+            // mButtonLogin
+            // 
+            this.mButtonLogin.AutoSize = false;
+            this.mButtonLogin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mButtonLogin.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.mButtonLogin.Depth = 0;
+            this.mButtonLogin.HighEmphasis = true;
+            this.mButtonLogin.Icon = null;
+            this.mButtonLogin.Location = new System.Drawing.Point(160, 293);
+            this.mButtonLogin.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.mButtonLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mButtonLogin.Name = "mButtonLogin";
+            this.mButtonLogin.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.mButtonLogin.Size = new System.Drawing.Size(115, 52);
+            this.mButtonLogin.TabIndex = 3;
+            this.mButtonLogin.Text = "LOGIN";
+            this.mButtonLogin.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.mButtonLogin.UseAccentColor = false;
+            this.mButtonLogin.UseVisualStyleBackColor = true;
+            // 
+            // mButtonSignUp
+            // 
+            this.mButtonSignUp.AutoSize = false;
+            this.mButtonSignUp.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mButtonSignUp.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.mButtonSignUp.Depth = 0;
+            this.mButtonSignUp.HighEmphasis = true;
+            this.mButtonSignUp.Icon = null;
+            this.mButtonSignUp.Location = new System.Drawing.Point(34, 293);
+            this.mButtonSignUp.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.mButtonSignUp.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mButtonSignUp.Name = "mButtonSignUp";
+            this.mButtonSignUp.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.mButtonSignUp.Size = new System.Drawing.Size(115, 52);
+            this.mButtonSignUp.TabIndex = 4;
+            this.mButtonSignUp.Text = "REGISTRATI";
+            this.mButtonSignUp.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.mButtonSignUp.UseAccentColor = false;
+            this.mButtonSignUp.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(307, 380);
+            this.Controls.Add(this.mButtonSignUp);
+            this.Controls.Add(this.mButtonLogin);
+            this.Controls.Add(this.mTextBoxPasswd);
+            this.Controls.Add(this.mTextBoxUsername);
+            this.Controls.Add(this.mLabelLogin);
             this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_None;
-            this.Name = "Form1";
+            this.Name = "Login";
             this.Padding = new System.Windows.Forms.Padding(3, 24, 3, 3);
             this.Text = "BdT - 0.0.2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialLabel mLabelLogin;
+        private MaterialSkin.Controls.MaterialTextBox mTextBoxPasswd;
+        private MaterialSkin.Controls.MaterialButton mButtonLogin;
+        private MaterialSkin.Controls.MaterialTextBox mTextBoxUsername;
+        private MaterialSkin.Controls.MaterialButton mButtonSignUp;
     }
 }
 
--- Borelli_BdT/view/Login.cs
+++ Borelli_BdT/view/Login.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using MaterialSkin;
 using MaterialSkin.Controls;
+using Borelli_BdT.presenter;
+using Borelli_BdT.utilities;
 
 namespace Borelli_BdT.view {
     public partial class Login : MaterialForm {
+        //private SignUp SignUpForm { get; set; } TODO
+        //private MainPage MainForm { get; set; } TODO
         public Login() {
             InitializeComponent();
+            FormManager.Init();
+            FormManager.AddForm(this);
+
+            //TODO
+            /*LoginPresenter p = new LoginPresenter(this);
+            mButtonLogin.Click += new EventHandler(p.OnLogin);
+            mButtonSignUp.Click += new EventHandler(p.OnSignUp);*/
+        }
+
+        public List<string> GetFieldsValue() {
+            return new List<string> {
+                mTextBoxUsername.Text,
+                mTextBoxPasswd.Text
+            };
+        }
+
+        public void NotValidUsername() {
+            mTextBoxUsername.Text = mTextBoxPasswd.Text = "";
+            MessageBox.Show("Inserire un nome utente esistente");
+        }
+
+        public void NotValidPassword() {
+            mTextBoxPasswd.Text = "";
+            MessageBox.Show("Password sbagliata");
+        }
+
+        public void UserIsNotYetConfirmed() {
+            mTextBoxUsername.Text = mTextBoxPasswd.Text = "";
+            MessageBox.Show("L'utente in questione è ancora in attesa di conferma da parte della segreteria");
+        }
+
+        public void DoLogin() { //TODO
+            /*MainForm = new MainPage(mTextBoxUsername.Text); TODO
+            this.Hide();
+            MainForm.ShowDialog();
+            this.Show();*/
+        }
+
+        public void ShowSignUpForm() { //TODO
+            /*SignUpForm = new SignUp(mTextBoxUsername.Text, mTextBoxPasswd.Text);
+            this.Hide();
+            SignUpForm.ShowDialog();
+            this.Show();*/
         }
     }
 }
