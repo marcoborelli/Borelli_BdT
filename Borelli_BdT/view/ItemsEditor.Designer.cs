b0f98371b6cd954478daa9d8ca67d2d580844a7f
marcoborelli <marco05.borelli@gmail.com>
2023-08-22 15:53:11 +0200

    view\ItemsEditor: aggiunta form contente la lista di tutte le zone-lavori disponibili con possibilità di aggiungerne altre.

---
+++ Borelli_BdT/view/ItemsEditor.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Borelli_BdT.view {
+    partial class ItemsEditor {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.mListBox = new MaterialSkin.Controls.MaterialListBox();
+            this.mButtonAdd = new MaterialSkin.Controls.MaterialButton();
+            this.mTextBoxNewItem = new MaterialSkin.Controls.MaterialTextBox();
+            this.mButtonSaveChanges = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // mListBox
+            // 
+            this.mListBox.BackColor = System.Drawing.Color.White;
+            this.mListBox.BorderColor = System.Drawing.Color.LightGray;
+            this.mListBox.Depth = 0;
+            this.mListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.mListBox.Location = new System.Drawing.Point(15, 27);
+            this.mListBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mListBox.Name = "mListBox";
+            this.mListBox.SelectedIndex = -1;
+            this.mListBox.SelectedItem = null;
+            this.mListBox.Size = new System.Drawing.Size(292, 515);
+            this.mListBox.TabIndex = 0;
+            // 
+            // mButtonAdd
+            // 
+            this.mButtonAdd.AutoSize = false;
+            this.mButtonAdd.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mButtonAdd.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.mButtonAdd.Depth = 0;
+            this.mButtonAdd.HighEmphasis = true;
+            this.mButtonAdd.Icon = null;
+            this.mButtonAdd.Location = new System.Drawing.Point(314, 86);
+            this.mButtonAdd.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.mButtonAdd.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mButtonAdd.Name = "mButtonAdd";
+            this.mButtonAdd.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.mButtonAdd.Size = new System.Drawing.Size(158, 36);
+            this.mButtonAdd.TabIndex = 1;
+            this.mButtonAdd.Text = "AGGIUNGI";
+            this.mButtonAdd.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.mButtonAdd.UseAccentColor = false;
+            this.mButtonAdd.UseVisualStyleBackColor = true;
+            // 
+            // mTextBoxNewItem
+            // 
+            this.mTextBoxNewItem.AnimateReadOnly = false;
+            this.mTextBoxNewItem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mTextBoxNewItem.Depth = 0;
+            this.mTextBoxNewItem.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.mTextBoxNewItem.Hint = "Nuovo valore";
+            this.mTextBoxNewItem.LeadingIcon = null;
+            this.mTextBoxNewItem.Location = new System.Drawing.Point(313, 27);
+            this.mTextBoxNewItem.MaxLength = 50;
+            this.mTextBoxNewItem.MouseState = MaterialSkin.MouseState.OUT;
+            this.mTextBoxNewItem.Multiline = false;
+            this.mTextBoxNewItem.Name = "mTextBoxNewItem";
+            this.mTextBoxNewItem.Size = new System.Drawing.Size(159, 50);
+            this.mTextBoxNewItem.TabIndex = 2;
+            this.mTextBoxNewItem.Text = "";
+            this.mTextBoxNewItem.TrailingIcon = null;
+            // 
+            // mButtonSaveChanges
+            // 
+            this.mButtonSaveChanges.AutoSize = false;
+            this.mButtonSaveChanges.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mButtonSaveChanges.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.mButtonSaveChanges.Depth = 0;
+            this.mButtonSaveChanges.HighEmphasis = true;
+            this.mButtonSaveChanges.Icon = null;
+            this.mButtonSaveChanges.Location = new System.Drawing.Point(314, 506);
+            this.mButtonSaveChanges.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.mButtonSaveChanges.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mButtonSaveChanges.Name = "mButtonSaveChanges";
+            this.mButtonSaveChanges.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.mButtonSaveChanges.Size = new System.Drawing.Size(158, 36);
+            this.mButtonSaveChanges.TabIndex = 3;
+            this.mButtonSaveChanges.Text = "SALVA MODIFICHE";
+            this.mButtonSaveChanges.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.mButtonSaveChanges.UseAccentColor = false;
+            this.mButtonSaveChanges.UseVisualStyleBackColor = true;
+            // 
+            // ItemsEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 554);
+            this.Controls.Add(this.mButtonSaveChanges);
+            this.Controls.Add(this.mTextBoxNewItem);
+            this.Controls.Add(this.mButtonAdd);
+            this.Controls.Add(this.mListBox);
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_None;
+            this.Name = "ItemsEditor";
+            this.Padding = new System.Windows.Forms.Padding(3, 24, 3, 3);
+            this.Text = "ItemsEditor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialListBox mListBox;
+        private MaterialSkin.Controls.MaterialButton mButtonAdd;
+        private MaterialSkin.Controls.MaterialTextBox mTextBoxNewItem;
+        private MaterialSkin.Controls.MaterialButton mButtonSaveChanges;
+    }
+}
---
+++ Borelli_BdT/view/ItemsEditor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using Borelli_BdT.utilities;
+
+namespace Borelli_BdT.view {
+    public partial class ItemsEditor : MaterialForm {
+        public enum Use {
+            Districts,
+            Jobs,
+        }
+
+        public Use FormUse { get; private set; }
+        public ItemsEditor(Use u) {
+            //TODO: mettere anche rinomina di un lavoro e eliminazione
+            InitializeComponent();
+            FormManager.AddForm(this);
+
+            FormUse = u;
+
+            //TODO
+            /*ItemsEditorPresenter p = new ItemsEditorPresenter(this);
+            mButtonAdd.Click += new EventHandler(p.OnAddButton);
+            mButtonSaveChanges.Click += new EventHandler(p.SaveChanges);*/
+        }
+
+        public void LoadItemsInListBox(List<string> items) {
+            mListBox.Items.Clear();
+
+            for (int i = 0; i < items.Count; i++) {
+                mListBox.Items.Add(new MaterialListBoxItem(items[i]));
+            }
+        }
+
+        public void ShowError(string error) {
+            MessageBox.Show($"{error}");
+            ResetTextBox();
+        }
+
+        public string GetNewItemText() {
+            return mTextBoxNewItem.Text.Trim().ToUpper();
+        }
+
+        public void ResetTextBox() {
+            mTextBoxNewItem.Text = String.Empty;
+        }
+
+        public void CloseForm() {
+            this.Close();
+        }
+    }
+}
